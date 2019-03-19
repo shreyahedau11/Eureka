76a2c7efa0743431ac3f17c776696c10dca7d49f
staysimplestaycool <48710316+staysimplestaycool@users.noreply.github.com>
2019-03-19 18:43:20 +0530

    Add files via upload

---
+++ EurekaServer/EurekaServer/src/main/java/com/example/EurekaServer/Controller2.java
@@ -0,0 +1,9 @@
+package com.example.EurekaServer;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+public class Controller2 {
+
+}
